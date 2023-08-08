a7f3135dde8b8150df8a11c413a338a47d3bd8c2
Gülce <gncglce@gmail.com>
2023-08-08 15:01:32 +0200

    add new topic and details

--- analysis.py
+++ analysis.py
@@ -8,13 +8,21 @@ from os import environ
 user = environ.get('USER')
 pw = environ.get('PW')
 # "Lebensbedingungen"
+
 data_str_rows = get_data(name="99911-0011", user=user, pw=pw)
 life1 = data_str_rows[3].split(';')[0].strip()  # topics
 life2 = data_str_rows[2].split(';')[0].split(',')[0].strip()  # countries
 life3 = data_str_rows[2].split(';')[0].split(',')[1].strip()  # years
 life3_val_str = data_str_rows[4].split(';')[3:]  # years-str
 life3_val_int = [int(leb_str) for leb_str in life3_val_str]  # years-int
+# "Gesundheit"
 
+data_str_rows2 = get_data(name="99911-0005", user=user, pw=pw)
+health1 = data_str_rows2[2].split(';')[0].strip()  # topics
+health2 = data_str_rows2[1].split(':')[1].split(',')[0].strip()  # countries
+health3 = data_str_rows2[1].split(':')[1].split(',')[1].strip(';').strip()  # years
+health3_val_str = data_str_rows2[3].split(';')[3:]  # years-str
+health3_val_int = [int(ges_str) for ges_str in health3_val_str]  # years-int
 
 
 
@@ -22,3 +30,6 @@ life3_val_int = [int(leb_str) for leb_str in life3_val_str]  # years-int
 
 df1, df2, topics_unique1, countries_unique1 = get_dataframe(topics=life1, countries=life2, years=life3_val_int,
                                                             input_data=data_str_rows[5:-11])
+
+df_1, df_2, topics_unique2, countries_unique2 = get_dataframe(topics=health1, countries=health2, years=health3_val_int,
+                                                              input_data=data_str_rows2[4:-12])
